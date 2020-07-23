98cd53d53382c840a5c67039922a4c507209c03b
Sasmita15 <43494328+Sasmita15@users.noreply.github.com>
2020-07-24 01:13:51 +0530

    Add files via upload
    POJO class of the project contains all attributes of the Customer table.

---
+++ Customer.java
@@ -0,0 +1,88 @@
+package com.sasmita.roombookingproject.modal;
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Version
+	private Integer version;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Date dob;
+	
+	private String email;
+	
+	private String password;
+
+	public Customer(int id, String firstName, String lastName, String userName, String password) {
+		
+		
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
