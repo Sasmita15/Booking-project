0037022938e7025b3dd107a780d43c5a168fb7f3
Sasmita15 <43494328+Sasmita15@users.noreply.github.com>
2020-07-24 01:17:43 +0530

    Add files via upload
    Main class of the project.

---
+++ RoomBookingProjectMain.java
@@ -0,0 +1,13 @@
+package com.sasmita.roombookingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingProjectMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBookingProjectMain.class, args);
+	}
+
+}
